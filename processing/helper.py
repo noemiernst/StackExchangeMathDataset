aaad6a07eaaadc8fb1b0613c73da852cfafbb152
Noemi Ernst <noemi.ernst@outlook.com>
2020-04-26 12:33:05 +0200

    Add sql table helper function

---
+++ processing/helper.py
@@ -0,0 +1,7 @@
+import sqlite3
+
+def write_table(database, table_name, dataframe, if_exists='replace', index=False):
+    DB = sqlite3.connect(database)
+    dataframe.to_sql(name=table_name, con=DB, if_exists=if_exists, index=index)
+    DB.close()
+    print("# wrote table '", table_name, "' to database '", database, "' with ", len(dataframe), " entries")
