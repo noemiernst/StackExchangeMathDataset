13113800c70f308dc20da132881ac23dafa6fbc7
Noemi Ernst <noemi.ernst@outlook.com>
2020-05-01 18:33:33 +0200

    Add formula analyzation

---
+++ analyze/formulas.py
@@ -0,0 +1,88 @@
+import latex2mathml
+import argparse
+import sqlite3
+import pandas as pd
+
+def number_of_equations(formulas):
+    count = 0
+    for formula in zip(formulas["Body"]):
+        if '=' in formula[0]:
+            count += 1
+    print('# equations: ', count, "  (formulas containing '=')")
+
+def number_of_single_character_formulas(formulas):
+    count = 0
+    for formula in zip(formulas["Body"]):
+        if len(formula[0]) == 1:
+            count += 1
+    print('# single character formulas: ', count)
+
+def greek_letters(formulas):
+    count = 0
+    symbols = [r'\alpha', r'\beta', r'\gamma', r'\delta', r'\epsilon', r'\zeta', r'\eta', r'\theta', r'\iota',
+               r'\kappa', r'\lambda', r'\mu', r'\nu', r'\xi', r'\omicron', r'\pi', r'\rho', r'\sigma', r'\tau',
+               r'\upsilon', r'\phi', r'\chi', r'\psi', r'\omega']
+    for formula in zip(formulas["Body"]):
+        f = (formula[0]).lower()
+        if any(sym in f for sym in symbols):
+            count += 1
+    print('# formulas containing greek letters: ', count)
+
+
+def super_subscripts(formulas):
+    super_count = 0
+    sub_count = 0
+    for formula in zip(formulas["Body"]):
+        if '^' in formula[0]:
+            super_count += 1
+        if '_' in formula[0]:
+            sub_count += 1
+    print('# formulas containing superscripts (^): ', super_count)
+    print('# formulas containing subscripts (_): ', sub_count)
+
+def sum_integral(formulas):
+    sum_count = 0
+    integral_count = 0
+    other_count = 0
+    symbols = [r'\prod', r'\bigcup', r'\bigcap', r'\iint', r'\iiint', r'\iiiint', r'\iiiiint', r'\idotsint']
+    for formula in zip(formulas["Body"]):
+        if r'\sum' in formula[0]:
+            sum_count += 1
+        if r'\int' in formula[0]:
+            integral_count += 1
+        if any(sym in formula[0] for sym in symbols):
+            other_count += 1
+
+    print(r'# formulas containing sums (\sum): ', sum_count)
+    print(r'# formulas containing integrals (\int): ', integral_count)
+    print(r'# formulas containing others (\prod, \bigcup,\bigcup, \iint, ...): ', other_count)
+
+def fractions(formulas):
+    count = 0
+    symbols = [r'\frac', r'\over', r'\cfrac']
+    for formula in zip(formulas["Body"]):
+        if any(sym in formula[0] for sym in symbols):
+            count += 1
+    print('# formulas containing fractions: ', count)
+
+def analyze_formulas(database, table):
+    DB = sqlite3.connect(database)
+    formulas = pd.read_sql('select Body from "'+ table+'"', DB)
+    DB.close()
+
+    print("# formulas: ", len(formulas["Body"]))
+
+    number_of_equations(formulas)
+    number_of_single_character_formulas(formulas)
+    greek_letters(formulas)
+    super_subscripts(formulas)
+    sum_integral(formulas)
+    fractions(formulas)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d","--database",default= "../output/physics.db", help = "input database with LaTeX formulas")
+    parser.add_argument("-t", "--table", default="Formulas_Posts", help= "name of table in database")
+    args = parser.parse_args()
+
+    analyze_formulas(args.database, args.table)
