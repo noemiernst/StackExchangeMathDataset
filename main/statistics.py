81c8e0918b69c3d1271c1b8db6223908833476fc
Noemi Ernst <noemi.ernst@outlook.com>
2020-06-06 15:05:54 +0200

    Add diagrams

--- main/statistics.py
+++ main/statistics.py
@@ -12,10 +12,12 @@ import sqlite3
 import pandas as pd
 import collections
 import re
+import resource
+import string
 
 
-# formulaid_postid = {formulaid: postid}, all_postids = []
-def formulas_per_post(formulaid_postid, all_postids):
+# formulaid_postid = {formulaid: postid}, all_postids = [], token_lengths = []
+def formulas_per_post(formulaid_postid, all_postids, token_lengths, site):
     posts = len(all_postids)
     formulas = len(formulaid_postid)
     print("# posts: "+ str(posts))
@@ -23,7 +25,7 @@ def formulas_per_post(formulaid_postid, all_postids):
     print("# average number of formulas per post: "+ format(formulas/posts, ".2f"))
     # percentage of posts with at least 1 formula
     unique_postids = len(set(formulaid_postid.values()))
-    print("# posts containing formulas: "+ str(unique_postids) + " -> "+ format(100* unique_postids/posts, ".2f") + "%")
+    print("# posts containing formulas: "+ str(unique_postids) + " -> "+ format(100* unique_postids/posts, ".2f") + "% of posts")
 
     # number of formulas in post {postid: #formulas}
     counter = Counter(formulaid_postid.values())
@@ -47,20 +49,37 @@ def formulas_per_post(formulaid_postid, all_postids):
 
     # make this into a histogram of number of formula distribution in questions, answers, posts and comments
     #counts_counter_below_100 = {k:v for (k,v) in counts_counter.items() if k <100}
-    plt.bar(list(counts_counter.keys()), counts_counter.values(), color='g',edgecolor='black', linewidth=1.2)
+    plt.subplot(2, 1, 1)
+    plt.subplots_adjust(left=0.15, hspace=0.55, wspace=0.3)
+    plt.bar(list(counts_counter.keys()), counts_counter.values(), color='g',edgecolor='black', linewidth=1)
+    plt.title("Formula Distribution of '"+ site + "' Dump")
+    plt.xlabel("Number of Formulas per Post")
+    plt.ylabel("Number of Posts")
+
+    top_filtered = sorted(token_lengths, reverse=True)[int(0.05*len(token_lengths)):]
+    plt.subplot(2, 1, 2)
+    plt.hist(top_filtered, bins=len(set(top_filtered)), color='g',edgecolor='black', linewidth=1, align='left')
+    plt.title("Formula Length Distribution in '"+ site + "' Dump")
+    plt.xlabel("Number of Tokens per Formula")
+    plt.ylabel("Number of Formulas")
     plt.show()
 
     # text pro gleichung: avg text of post with formula
 
 
 def common_words(docs, x):
-    docs = [re.sub(r'(\$\$.*?\$\$|\$.*?\$)|<.*?>', '', d) for d in docs]
-    vectorizer = CountVectorizer(min_df=0.75, max_features=x)
-    word_count_vector=vectorizer.fit_transform(docs)
-    count_list =word_count_vector.toarray().sum(axis=0)
+    docs = [re.sub(r'(\$\$.*?\$\$|\$.*?\$)|<.*?>|\\amp', ' ', d) for d in docs]
+    table = str.maketrans('', '', string.punctuation)
+    temp = []
+    for words in docs:
+        temp.append(words.translate(table).lower())
+    vectorizer = CountVectorizer(max_df=0.75, max_features=x, stop_words='english')
+    word_count_vector = vectorizer.fit_transform(temp)
+    count_list = word_count_vector.toarray().sum(axis=0)
     word_list = vectorizer.get_feature_names()
     words = dict(zip(word_list, count_list))
-    print(words)
+    words = collections.OrderedDict(sorted(words.items(), key=lambda item: item[1], reverse=True))
+    print(pd.DataFrame(words.items(), columns=['Word', 'idf']).to_string())
 
 def common_tokens(tokens, x):
     token_dict = {}
@@ -71,7 +90,7 @@ def common_tokens(tokens, x):
             token_dict[token] = 1
     counter = Counter(tokens)
     topx = counter.most_common(x)
-    print(topx)
+    print(pd.DataFrame(topx, columns=['Token', 'Occurences']).to_string())
 
 def all_tokens(formulas):
     tokens = []
@@ -88,13 +107,18 @@ def main(filename_dumps, database):
 
     for site in sites:
         DB = sqlite3.connect(database)
-        formulas_posts = pd.read_sql('select FormulaId, PostId from "FormulasPosts" where Site="'+site+'"', DB)
-        all_questions = pd.read_sql('select QuestionId from "QuestionTags" where Site="'+site+'"', DB)
-        all_answers = pd.read_sql('select AnswerId from "AnswerMeta" where Site="'+site+'"', DB)
+        formulas_posts = pd.read_sql('select FormulaId, PostId, TokenLength from "FormulasPosts" where Site="'+site+'"', DB)
+        question_ids = pd.read_sql('select QuestionId from "QuestionTags" where Site="'+site+'"', DB)
+        answer_ids = pd.read_sql('select AnswerId from "AnswerMeta" where Site="'+site+'"', DB)
         DB.close()
-        all_posts = all_questions["QuestionId"] + all_answers["AnswerId"]
+        post_ids = question_ids["QuestionId"] + answer_ids["AnswerId"]
+        question_ids.pop("QuestionId")
+        answer_ids.pop("AnswerId")
 
-        formulas_per_post(dict(zip(formulas_posts["FormulaId"], formulas_posts["PostId"])), all_posts)
+        formulas_per_post(dict(zip(formulas_posts["FormulaId"], formulas_posts["PostId"])), post_ids, list(formulas_posts["TokenLength"]), site)
+        formulas_posts.pop("FormulaId")
+        formulas_posts.pop("PostId")
+        print("max memory usage: " + format((resource.getrusage(resource.RUSAGE_SELF).ru_maxrss)/pow(2,30), ".3f")+ " GigaByte")
 
         DB = sqlite3.connect(database)
         question_texts = pd.read_sql('select Title, Body from "QuestionText" where Site="'+site+'"', DB)
@@ -102,12 +126,18 @@ def main(filename_dumps, database):
         DB.close()
 
         common_words(list(question_texts["Title"]) + list(question_texts["Body"]) + list(answer_texts["Body"]), 100)
+        question_texts.pop("Title")
+        question_texts.pop("Body")
+        answer_texts.pop("Body")
+        print("max memory usage: " + format((resource.getrusage(resource.RUSAGE_SELF).ru_maxrss)/pow(2,30), ".3f")+ " GigaByte")
 
         DB = sqlite3.connect(database)
         formulas_posts = pd.read_sql('select Body from "FormulasPosts" where Site="'+site+'"', DB)
         DB.close()
 
         common_tokens(all_tokens(list(formulas_posts["Body"])), 100)
+        formulas_posts.pop("Body")
+        print("max memory usage: " + format((resource.getrusage(resource.RUSAGE_SELF).ru_maxrss)/pow(2,30), ".3f")+ " GigaByte")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
