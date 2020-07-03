f6d266dab56b1ccac273f87a059e22bf068b5101
Noemi Ernst <noemi.ernst@outlook.com>
2020-07-03 10:58:06 +0200

    Add threading

--- main/formula_parsing/formulas_to_mml.py
+++ main/formula_parsing/formulas_to_mml.py
@@ -8,36 +8,47 @@ import subprocess
 from dump_processing.helper import write_table
 from dump_processing.helper import update_table
 from dump_processing.helper import progress_bar
+from threading import Thread
 
+
+def threadFunc(call, body, cmml, id):
+    p2 = subprocess.Popen(call, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    output, error = p2.communicate(input=body.encode())
+    if error == b'':
+        cmml[id] = output.decode("utf-8")
+    else:
+        cmml[id] = ""
 
 def formulas_to_cmml(database, table, site):
     DB = sqlite3.connect(database)
     formulas = pd.read_sql('select tex.FormulaId, tex.LaTeXBody from "'+ table +'" tex LEFT JOIN "' + table + 'MathML" ml ON ml.FormulaId = tex.FormulaId WHERE ml.ContentMathML IS NULL AND tex.Site="'+site+'"', DB)
     DB.close()
 
+    threaded = []
     cmml = {}
     sites = []
     count = 0
+    th = {}
     for formula, body in zip(formulas["FormulaId"], formulas["LaTeXBody"]):
         count += 1
-        use_shell = ('Windows' in platform.system())
-        p2 = subprocess.Popen(
-                ['latexmlmath', '--cmml=-', #'--preload=amsmath', '--preload=amsfonts',
-                 '-'], shell=use_shell, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+        th[formula] = Thread(target=threadFunc, args=(['latexmlmath', '--cmml=-', '-'], body, cmml, formula))
+        threaded.append(formula)
+        th[formula].start()
 
-        output, error = p2.communicate(input=body.encode())
-        if error == b'':
-            cmml[formula] = output.decode("utf-8")
-        else:
-            cmml[formula] = ""
-        sites.append(site)
-
-        if len(sites) > 9:
-            df = pd.DataFrame({"FormulaId": list(cmml.keys()), "Site": site, "ContentMathML": list(cmml.values())})
+        if(len(threaded) > 19):
+            for formula in threaded:
+                th[formula].join()
+                sites.append(site)
+            threaded = []
+            keys = [i[0] for i in sorted(cmml.items(), key=lambda item: item[0])]
+            values = [i[1] for i in sorted(cmml.items(), key=lambda item: item[0])]
+            df = pd.DataFrame({"FormulaId": keys, "Site": site, "ContentMathML": values})
             update_table(database, table+"MathML", df, "FormulaId")
             cmml = {}
             sites = []
             progress_bar(count, len(formulas["FormulaId"]), "Formulas of " + table + " in " + site, 40)
+
     if count != 0:
         df = pd.DataFrame({"FormulaId": list(cmml.keys()), "Site": site, "ContentMathML": list(cmml.values())})
         update_table(database, table+"MathML", df, "FormulaId")
@@ -50,25 +61,25 @@ def formulas_to_pmml(database, table, site):
     formulas = pd.read_sql('select tex.FormulaId, tex.LaTeXBody from "'+ table +'" tex LEFT JOIN "' + table + 'MathML" ml ON ml.FormulaId = tex.FormulaId WHERE ml.PresentationMathML IS NULL AND tex.Site="'+site+'"', DB)
     DB.close()
 
+    threaded = []
     pmml = {}
     sites = []
     count = 0
+    th = {}
     for formula, body in zip(formulas["FormulaId"], formulas["LaTeXBody"]):
         count += 1
-        use_shell = ('Windows' in platform.system())
-        p2 = subprocess.Popen(
-                ['latexmlmath', '--pmml=-', #'--preload=amsmath', '--preload=amsfonts',
-                 '-'], shell=use_shell, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+        th[formula] = Thread(target=threadFunc, args=(['latexmlmath', '--pmml=-', '-'], body, pmml, formula))
+        threaded.append(formula)
+        th[formula].start()
 
-        output, error = p2.communicate(input=body.encode())
-        if error == b'':
-            pmml[formula] = output.decode("utf-8")
-        else:
-            pmml[formula] = ""
-        sites.append(site)
-
-        if len(sites) > 9:
-            df = pd.DataFrame({"FormulaId": list(pmml.keys()), "Site": site, "PresentationMathML": list(pmml.values())})
+        if(len(threaded) > 19):
+            for formula in threaded:
+                th[formula].join()
+                sites.append(site)
+            threaded = []
+            keys = [i[0] for i in sorted(pmml.items(), key=lambda item: item[0])]
+            values = [i[1] for i in sorted(pmml.items(), key=lambda item: item[0])]
+            df = pd.DataFrame({"FormulaId": keys, "Site": site, "PresentationMathML": values})
             update_table(database, table+"MathML", df, "FormulaId")
             pmml = {}
             sites = []
@@ -85,46 +96,44 @@ def formulas_to_both_ml(database, table, site):
     DB.close()
 
     ids = []
-    cmml = []
-    pmml = []
+    threaded = []
+    pmml = {}
+    cmml = {}
     sites = []
     count = 0
+    th_p = {}
+    th_c = {}
     for formula, body in zip(formulas["FormulaId"], formulas["LaTeXBody"]):
         count += 1
-        use_shell = ('Windows' in platform.system())
-        p2 = subprocess.Popen(
-                ['latexmlmath', '--cmml=-', #'--preload=amsmath', '--preload=amsfonts',
-                 '-'], shell=use_shell, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
-        output_c, error = p2.communicate(input=body.encode())
-        if error != b'':
-            output_c = ""
-        else:
-            output_c = output_c.decode("utf-8")
 
-        p2 = subprocess.Popen(
-                ['latexmlmath', '--pmml=-', #'--preload=amsmath', '--preload=amsfonts',
-                 '-'], shell=use_shell, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
-        output_p, error = p2.communicate(input=body.encode())
-        if error != b'':
-            output_p = ""
-        else:
-            output_p = output_p.decode("utf-8")
+        th_p[formula] = Thread(target=threadFunc, args=(['latexmlmath', '--cmml=-', '-'], body, cmml, formula))
+        th_p[formula].start()
 
+        th_c[formula] = Thread(target=threadFunc, args=(['latexmlmath', '--pmml=-', '-'], body, pmml, formula))
+        th_c[formula].start()
+
+        threaded.append(formula)
         ids.append(formula)
-        cmml.append(output_c)
-        pmml.append(output_p)
         sites.append(site)
 
-        if len(sites) > 9:
+        if(len(threaded) > 9):
+            for formula in threaded:
+                th_p[formula].join()
+                th_c[formula].join()
+                sites.append(site)
+            threaded = []
+            cmml = [i[1] for i in sorted(cmml.items(), key=lambda item: item[0])]
+            pmml = [i[1] for i in sorted(pmml.items(), key=lambda item: item[0])]
+            ids = sorted(ids)
             df = pd.DataFrame({"FormulaId": ids, "Site": site, "ContentMathML": cmml, "PresentationMathML": pmml})
             update_table(database, table+"MathML", df, "FormulaId")
             ids = []
-            cmml = []
-            pmml = []
+            pmml = {}
+            cmml = {}
             sites = []
             progress_bar(count, len(formulas["FormulaId"]), "Formulas of " + table + " in " + site, 40)
     if count != 0:
-        df = pd.DataFrame({"FormulaId": ids, "Site": site, "ContentMathML": cmml, "PresentationMathML": pmml})
+        df = pd.DataFrame({"FormulaId": ids, "Site": site, "ContentMathML": list(cmml.values()), "PresentationMathML": list(pmml.values())})
         update_table(database, table+"MathML", df, "FormulaId")
         progress_bar(count, len(formulas["FormulaId"]), "Formulas of " + table + " in " + site, 40)
     return cmml
